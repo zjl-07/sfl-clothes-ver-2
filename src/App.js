5e9e815cd7034c513db827d6597045476e0095e8
EmildaZhang <emildazhg07@gmail.com>
2019-09-06 14:52:25 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Homepage from "./homepage";
+import "./styles/main.scss";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homepage.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Homepage = props => {
+  return (
+    <div className="homepage">
+      <div className="menu_items">
+        <div className="menu_items-item">
+          <div className="content">
+            <div className="title">Sneakers</div>
+            <span className="sub-title">SHOP NOW!</span>
+          </div>
+        </div>
+        <div className="menu_items-item">
+          <div className="content">
+            <div className="title">Cardigan</div>
+            <span className="sub-title">SHOP NOW!</span>
+          </div>
+        </div>
+        <div className="menu_items-item">
+          <div className="content">
+            <div className="title">Cardigan</div>
+            <span className="sub-title">SHOP NOW!</span>
+          </div>
+        </div>
+        <div className="menu_items-item">
+          <div className="content">
+            <div className="title">Cardigan</div>
+            <span className="sub-title">SHOP NOW!</span>
+          </div>
+        </div>
+        <div className="menu_items-item">
+          <div className="content">
+            <div className="title">Cardigan</div>
+            <span className="sub-title">SHOP NOW!</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
