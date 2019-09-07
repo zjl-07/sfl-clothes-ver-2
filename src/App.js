b886fa472fb6678de386201bdc11dbdf4926d328
EmildaZhang <emildazhg07@gmail.com>
2019-09-07 13:37:57 +0700

    building header feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "pages/homepage";
 import ShopPage from "pages/shoppage";
+import Header from "./components/header/header";
 import "styles/main.scss";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
