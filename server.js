0a29de34d2ac127a3c2c3212f6d438b2878a3422
EmildaZhang <emildazhg07@gmail.com>
2019-12-27 09:07:08 +0700

    fixing bug server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.listen(port, error => {
 });
 
 app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "...", "build", "service-worker.js"));
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
 app.post("/payment", (req, res) => {
